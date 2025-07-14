21e21fce3030e658dcbdba0a340be46b4979dda2
Sebastian Pokładek <sebpok228@gmail.com>
2025-07-15 01:43:23 +0200

    db conncection

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 
 	v1 "github.com/sebpok/email-sender/internal/api/v1"
+	"github.com/sebpok/email-sender/internal/database"
 )
 
 func main() {
+	database.Connect()
+
 	r := v1.NewRouter()
 	http.ListenAndServe(":8080", r)
 }
--- backend/internal/database/connection.go
+++ backend/internal/database/connection.go
@@ -1,5 +1,31 @@
 package database
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+var conn *sql.DB
+
 func Connect() {
-	
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		"user",
+		"password",
+		"localhost",
+		"5432",
+		"email_sender",
+		"disable",
+	)
+
+	c, err := sql.Open("postgres", connStr)	
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB CONN IS WORKING")
+	conn = c
+}
+
+func GetConnection() *sql.DB {
+	return conn
 }
