86ff57a5bb6ad918006177852eb2ed5da3539b88
Sebastian Pokładek <sebpok228@gmail.com>
2025-07-16 21:50:16 +0200

    init

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	v1 "github.com/sebpok/email-sender/internal/api/v1"
@@ -11,5 +12,6 @@ func main() {
 	database.Connect()
 
 	r := v1.NewRouter()
+	log.Println("Server started on: localhost:8080")
 	http.ListenAndServe(":8080", r)
 }
